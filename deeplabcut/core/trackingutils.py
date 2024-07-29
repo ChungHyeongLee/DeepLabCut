0c7410f64535365ae985320701fa05d0fb1e53ac
ChungHyeongLee <39398450+ChungHyeongLee@users.noreply.github.com>
2024-07-30 01:21:09 +0900

    swkim___

--- deeplabcut/core/trackingutils.py
+++ deeplabcut/core/trackingutils.py
@@ -115,7 +115,7 @@ class Ellipse:
         dist = math.sqrt(
             (self.x - other_ellipse.x) ** 2 + (self.y - other_ellipse.y) ** 2
         )
-        ###
+        #######
         cost1 = 1 - min(dist / max_dist, 1)
         cost2 = abs(math.cos(self.theta - other_ellipse.theta))
         return 0.8 * cost1 + 0.2 * cost2 * cost1
